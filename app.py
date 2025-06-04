fd23df71dc7632c88e0d2e9ce7861f7f52699dc8
Himadri Bhattacharya <hbhattacharya3@gmail.com>
2025-06-04 21:01:31 +0530

    Major changes

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ api_key = st.secrets["api_key"]  # or paste directly for local testing
 
 sentiment_pipeline = pipeline(
     "sentiment-analysis",
-    model="distilbert/distilbert-base-uncased-finetuned-sst-2-english",
-    revision="714eb0f"
 )
 
 
